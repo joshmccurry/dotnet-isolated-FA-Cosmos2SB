c0bc2d838fee952d68a07b5d416878af5395a940
Josh McCurry <joshmccurry@microsoft.com>
2025-02-12 10:29:34 -0500

    Update Program.cs for Identity Suggestion

--- Program.cs
+++ Program.cs
@@ -1,15 +1,14 @@
-using jm_func_cosmosdb_to_sb.Services;
+using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Builder;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using jm_func_cosmosdb_to_sb.Models;
-using Microsoft.Azure.Functions.Worker;
+using jm_func_cosmosdb_to_sb.Services;
 
 var builder = FunctionsApplication.CreateBuilder(args);
 
 builder.ConfigureFunctionsWebApplication();
 
-// Application Insights isn't enabled by default. See https://aka.ms/AAt8mw4.
 builder.Services
      .AddApplicationInsightsTelemetryWorkerService()
      .ConfigureFunctionsApplicationInsights();
@@ -17,8 +16,14 @@ builder.Services
 builder.Services.AddSingleton(new ServiceBusCSDetails() {
     ConnectionString = Environment.GetEnvironmentVariable("ServiceBusConnectionString"),
     QueueName = Environment.GetEnvironmentVariable("ServiceBusQueueName")
-}
-    );
+});
+
+//For Managed Identity Connections
+//builder.Services.AddSingleton(new ServiceBusIdentityDetails() {
+//    ServiceBusNamespace = Environment.GetEnvironmentVariable("ServiceBusNameSpace"),
+//    QueueName = Environment.GetEnvironmentVariable("ServiceBusQueueName")
+//});
+
 builder.Services.AddSingleton<IServiceBusClientService, ServiceBusClientService>();
 
 builder.Build().Run();
